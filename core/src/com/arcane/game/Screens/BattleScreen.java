78dcc9031aa5155c34c3c90ca77b1acb3a1e3bfa
shizukou-beat <119555723+shizukou-beat@users.noreply.github.com>
2024-06-01 21:22:59 +0800

    Things added:  1. TitleScreen  2. Button classes  3. BattleScreen

--- core/src/com/arcane/game/Screens/BattleScreen.java
+++ core/src/com/arcane/game/Screens/BattleScreen.java
@@ -7,6 +7,8 @@ import com.arcane.game.ArcaneOdyssey;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -44,6 +46,11 @@ public class BattleScreen extends ArcaneScreen{
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Texture background = new Texture(Gdx.files.internal("Backgrounds/Grass.png"));
+        Batch bg = new SpriteBatch();
+        bg.begin();
+        bg.draw(background, 0, 0, width, height);
+        bg.end();
         stage.act();
         stage.draw();
     }
